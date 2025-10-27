a1b4ba8641dcafe10bb3bf02e2ec38e6efa76b86
vilson.junior <vilson.teixeira@lighthouseit.com.br>
2025-10-27 16:30:06 -0300

    teste: adicionado report para evidenciar testes

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,15 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     watchForFileChanges: false,
     viewportHeight: 1400,
     viewportWidth: 1600,
     baseUrl: "https://www.saucedemo.com/"
+  },
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,    
   },
 });
