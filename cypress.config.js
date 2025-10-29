8f35c81b8d90e4281791d2275f9c18b911dce3a0
vilson.junior <vilson.teixeira@lighthouseit.com.br>
2025-10-29 13:42:30 -0300

    teste: editado cypress.yml teste readme 11

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
     },
     watchForFileChanges: false,
